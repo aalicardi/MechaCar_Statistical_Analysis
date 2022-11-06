a9bbe975fc09e2ac2ef99535b352e899aa81f323
Annika Alicardi <520alicardi@gmail.com>
2022-11-06 01:13:19 -0500

    deliverable 3

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -8,3 +8,9 @@ summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_cleara
 suspension_coil <- read.csv(file='suspension_coil.csv',check.names=F,stringsAsFactors = F) #import and read csv as dataframe
 total_summary <- suspension_coil %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups='keep') 
 lot_summary <- suspension_coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups='keep')
+
+# DELIVERABLE 3
+t.test(Suspension$PSI,mu = 1500)
+t.test(subset(suspension_coil,Manufacturing_Lot=="Lot1")$PSI,mu = 1500)
+t.test(subset(suspension_coil,Manufacturing_Lot=="Lot2")$PSI,mu = 1500)
+t.test(subset(suspension_coil,Manufacturing_Lot=="Lot3")$PSI,mu = 1500)
